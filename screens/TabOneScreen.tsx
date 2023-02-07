1b3ae4862a58aff590fb0ad0047674c2112936de
Joel Storr <joel.storr@ewe.net>
2023-02-07 19:35:06 +0100

    Added GraphQL Query

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,8 +1,48 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+
+import { gql } from "@apollo/client";
+
+
+
+
+
+const query = gql`
+  query SearchBooks($q: String) {
+    googleBooksSearch(q: $q, country: "US") {
+      items {
+        id
+        volumeInfo {
+          authors
+          averageRating
+          description
+          imageLinks {
+            thumbnail
+          }
+          title
+          subtitle
+          industryIdentifiers {
+            identifier
+            type
+          }
+        }
+      }
+    }
+    openLibrarySearch(q: $q) {
+      docs {
+        author_name
+        title
+        cover_edition_key
+        isbn
+      }
+    }
+  }
+`;
+
+
+
+
 
 export default function TabOneScreen() {
   return (
