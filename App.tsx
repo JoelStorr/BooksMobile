eec564a2000a9f326ebcd311ce67905f2b451f27
Joel Storr <joel.storr@ewe.net>
2023-02-07 19:53:10 +0100

    Fixed API URL Error

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from "@apollo/client";
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
@@ -10,7 +10,7 @@ import Navigation from './navigation';
 import { ENV } from './env.js';
 
 const client = new ApolloClient({
-  uri: " https://buurgaabo.stepzen.net/api/calico-uakari/__graphql",
+  uri: "https://buurgaabo.stepzen.net/api/calico-uakari/__graphql",
   headers:{
     Authorization: `Apikey ${ENV.APIKey}`
   },
