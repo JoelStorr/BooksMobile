db895a2ba859ebf956e628347c51e2218343af88
Joel Storr <joel.storr@ewe.net>
2023-02-08 17:53:52 +0100

    Added Context Methods to Books context to Toggle saved and unsaved Books.

--- context/MyBooksProvider.tsx
+++ context/MyBooksProvider.tsx
@@ -1,14 +1,52 @@
-import { createContext} from "react";
+import {createContext, useContext, ReactNode, useState} from "react";
 
 
-const MyBooksContext = createContext({});
+ const MyBooksContext = createContext({});
 
-const MyBooksProvider = ({children}) =>{
+ type Props = {
+     children: ReactNode,
+}
+
+const MyBooksProvider = ({ children }: Props) =>{
+
+     const [savedBooks, setSavedBooks] = useState<Book[]>([]);
+
+
+     const areBooksTheSame = (a:Book, b:Book ) =>{
+         return JSON.stringify(a) === JSON.stringify(b)
+     }
+
+     const isBookSaved = (book: Book)=>{
+         //.some looks for at least one match
+         return savedBooks.some(
+             (savedBook)=> areBooksTheSame(savedBook, book)
+         );
+     }
+     const onToggleSave = (book: Book) =>{
+
+        if(isBookSaved(book)){
+            //remove
+            setSavedBooks(
+                (books) => books.filter(
+                    (savedBook)=> JSON.stringify(savedBook) !== JSON.stringify(book)
+                )
+            );
+        }else{
+            //add to save
+            setSavedBooks((books)=> [book, ...books]);
+        }
+
+
+     }
+
     return(
-        <MyBooksContext.Provider value={{test: 'a'}} >
+        <MyBooksContext.Provider value={{onToggleSave}} >
             {children}
         </MyBooksContext.Provider>
     )
 }
+
+
+export const useMyBooks = ()=> useContext(MyBooksContext);
 
 export default  MyBooksProvider;
